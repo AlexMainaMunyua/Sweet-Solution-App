7ba112c6c05740ed33d9978c706afc668ee7282d
AlexMainaMunyua <alexmaina79@gmail.com>
2022-04-17 15:41:38 +0300

    refactor: update plugin

--- lib/pages/Widgets/CustomTextField.dart
+++ lib/pages/Widgets/CustomTextField.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-@immutable
-// ignore: must_be_immutable
+
 class CustomTextField extends StatelessWidget {
   final TextEditingController? controller;
   final IconData? data;
